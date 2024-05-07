4ce42fc5e2a7b0feb3e4042138d95fd53dab98e1
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2024-05-07 11:38:34 -0500

    proyecto creado

---
+++ backend/appbackend/src/test/java/co/litethinking/core/appbackend/AppbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.litethinking.core.appbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
